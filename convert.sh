39ddc23557af9a16cb30babf69401dc824ab197d
AnonymousSeal <>
2021-11-09 13:28:56 +0100

    small bug fix; added path variable

--- convert.sh
+++ convert.sh
@@ -2,8 +2,10 @@
 
 shopt -s extglob
 
+path='./audiobooks'
 activation_bytes=''
-if [$activation_bytes = '']
+
+if ["$activation_bytes" = '']
 then
   echo "open the file and fill in the activation bytes"
   echo "hint: you can find out the activation bytes connected to your audible account"
@@ -15,11 +17,12 @@ function obtain_name(){
     exiftool $1 | egrep "^Short Title" | cut -c 35-
 }
 
-for f in ./audiobooks/*.aav; do
+for f in "$path/*.aav"; do
    title="$(obtain_name $f)"
    title="${title//[ ,.:-]/_}"
    title="${title//\'/}"
    title="${title//+(_)/_}"
    title="converted/$title.m4b"
-   ffmpeg -y -activation_bytes $activation_bytes -i $f -map_metadata 0 -c:v copy -disposition:v:0 attached_pic -id3v2_version 3 -codec:a copy $title
+   ffmpeg -y -activation_bytes $activation_bytes -i $f -map_metadata 0 \
+   -c:v copy -disposition:v:0 attached_pic -id3v2_version 3 -codec:a copy $title
 done
